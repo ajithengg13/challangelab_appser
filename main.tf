43d2cfb7075ec4b3d6f6c15a37110bf6aaeba0d8
ajithengg13 <84135675+ajithengg13@users.noreply.github.com>
2021-09-22 19:02:59 +0530

    1.2

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "appser" {
   location = azurerm_resource_group.project2.location
   resource_group_name = azurerm_resource_group.project2.name
   kind = "Linux"
-  reserved = false  
+  reserved = true  
   sku {
     tier = "Basic"
     size = "B1"
